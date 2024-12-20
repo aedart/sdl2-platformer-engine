f8bd8f0008f4b0ee87146bd1081e82886fa2f322
alin <alin@rspsystems.com>
2024-12-20 14:55:08 +0100

    Add dependencies cmake script (incomplete)

---
+++ dependencies.cmake
@@ -0,0 +1,13 @@
+# --------------------------------------------------------------------------------------------------
+# Loads all 3rd party dependencies
+# --------------------------------------------------------------------------------------------------
+
+# Debug
+message("Resolving dependencies for ${PROJECT_NAME}")
+
+# Ensure that git is available
+find_package(Git REQUIRED)
+
+# Other dependencies...
+# include("sdl2")
+# include("sdl2-image")
