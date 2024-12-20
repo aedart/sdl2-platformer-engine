e40fd620bef5129ebd68a36bc3526f03d6191326
alin <alin@rspsystems.com>
2024-12-20 19:34:09 +0100

    Replace SDL checks and init the engine

--- src/main.cpp
+++ src/main.cpp
@@ -2,6 +2,7 @@
 #include <SDL2/SDL.h>
 #include <SDL2/SDL_image.h>
 #include <SDL2/SDL_ttf.h>
+#include "Core/Engine.h"
 
 /**
  * C++ 2D Platform Engine, based on SDL
@@ -15,46 +16,56 @@
  */
 int main(int argc, char *argv[])
 {
-    // Define the SDL flags for initialisation, which must be ready
-    // before able to start a game loop.
-    constexpr int SDL_FLAGS = SDL_INIT_VIDEO | SDL_INIT_TIMER;
+    // // Define the SDL flags for initialisation, which must be ready
+    // // before able to start a game loop.
+    // constexpr int SDL_FLAGS = SDL_INIT_VIDEO | SDL_INIT_TIMER;
+    //
+    // // Define the SDL Image flags for initialisation.
+    // constexpr int SDL_IMAGE_FLAGS = IMG_INIT_PNG;
 
-    // Define the SDL Image flags for initialisation.
-    constexpr int SDL_IMAGE_FLAGS = IMG_INIT_PNG;
+    // The title of the game screen (window)
+    const auto TITLE = "C++ 2D Platform Engine, v0.1.0";
+
+    // Screen width in pixels
+    constexpr int SCREEN_WIDTH = 960;
+
+    // Screen height in pixels
+    constexpr int SCREEN_HEIGHT = 640;
 
     // -------------------------------------------------------------------------
     // Initialisation
     // -------------------------------------------------------------------------
 
-    // Abort if SDL failed to initialise.
-    if (SDL_Init(SDL_FLAGS) != 0) {
-        std::cerr << "SDL Init Error: " << SDL_GetError() << std::endl;
-        return EXIT_FAILURE;
-    }
+    Engine& engine = Engine::getInstance();
 
-    // Abort if SDL Image failed to initialise
-    if (IMG_Init(SDL_IMAGE_FLAGS) == 0) {
-        std::cerr << "SDL Image Init Error: " << SDL_GetError() << std::endl;
-        return EXIT_FAILURE;
-    }
-
-    // Abort if SDL ttf failed to initialise
-    if (TTF_Init() == -1) {
-        std::cerr << "TTF Init Error: " << TTF_GetError() << std::endl;
+    if (!engine.init(TITLE, SCREEN_WIDTH, SCREEN_HEIGHT)) {
+        std::cerr << "Failed to initialize engine!" << std::endl;
         return EXIT_FAILURE;
     }
 
     // -------------------------------------------------------------------------
-    // TODO
+    // Game loop
     // -------------------------------------------------------------------------
+
+    while(engine.isRunning())
+    {
+        // Collect all events, e.g. user inputs, etc.
+        engine.event();
+
+        // Perform evt. physics calculations, ...etc
+        engine.update();
+
+        // Finally, render
+        engine.render();
+    }
+
+    engine.clean();
 
     // -------------------------------------------------------------------------
     // Destroy
     // -------------------------------------------------------------------------
 
-    TTF_Quit();
-    IMG_Quit();
-    SDL_Quit();
+    Engine::destroy();
 
     return EXIT_SUCCESS;
 }
