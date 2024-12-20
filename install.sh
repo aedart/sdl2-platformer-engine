ae684ded34edae3bd0d3acab3321603a02f7b0cd
alin <alin@rspsystems.com>
2024-12-20 15:00:03 +0100

    Add install.sh

---
+++ install.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+
+# Runtime dependencies for this project
+sudo apt install -y git \
+                    build-essential \
+                    pkg-config \
+                    cmake \
+                    cmake-data \
+                    libsdl2-dev \
+                    libsdl2-gfx-dev \
+                    libsdl2-image-dev \
+                    libsdl2-ttf-dev
