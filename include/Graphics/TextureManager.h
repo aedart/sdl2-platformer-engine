6e213f4953fad8495319ba1bf14aef0485351ce4
alin <alin@rspsystems.com>
2024-12-21 13:56:43 +0100

    Add public API methods for the Texture Manager

--- include/Graphics/TextureManager.h
+++ include/Graphics/TextureManager.h
@@ -1,6 +1,10 @@
 #ifndef GRAPHICS_TEXTURE_MANAGER_H
 #define GRAPHICS_TEXTURE_MANAGER_H
 
+#include <SDL2/SDL.h>
+
+#include <string>
+
 /**
  * TextureManager
  *
@@ -37,6 +41,47 @@ class TextureManager
          * Destroy the texture manager instance
          */
         static void destroy();
+
+        /**
+         * Load a texture from the filesystem
+         *
+         * @param id The id of the texture
+         * @param file Path to the texture file
+         *
+         * @return
+         */
+        bool load(std::string id, std::string file);
+
+        /**
+         * Clean all textures from the memory
+         */
+        void clean();
+
+        /**
+         * Draw a texture
+         *
+         * @param id The id of the texture
+         * @param x X-axis offset in pixels
+         * @param y Y-axis offset in pixels
+         * @param width The width of the texture to be drawn in pixels
+         * @param height The height of the texture to be drawn in pixels
+         * @param flip Flip mode
+         */
+        void draw(
+            std::string id,
+            int x,
+            int y,
+            int width,
+            int height,
+            SDL_RendererFlip flip = SDL_FLIP_NONE
+        );
+
+        /**
+         * Drop (remove) texture from this manager
+         *
+         * @param id
+         */
+        void drop(std::string id);
 
     protected:
         /**
