a12de3fc7b746dc1bdb3980bc24165b345ad3492
alin <alin@rspsystems.com>
2024-12-25 18:22:40 +0100

    Add Rigid Body
    Component deals with the actual physics of a game object, character or similar. It does not yet handle collisions,...

---
+++ include/Physics/RigidBody.h
@@ -0,0 +1,186 @@
+#ifndef PHYSICS_RIGID_BODY_H
+#define PHYSICS_RIGID_BODY_H
+
+#include "Physics/Vector2D.h"
+
+/**
+ * Default mass of a body
+ */
+constexpr float DEFAULT_MASS = 1.0f;
+
+/**
+ * Default gravity of a body
+ */
+constexpr float DEFAULT_GRAVITY = 9.81f;
+
+/**
+ * Rigid Body
+ *
+ * @author Alin Eugen Deac <aedart@gmail.com>
+ */
+class RigidBody
+{
+    public:
+        /**
+         * Constructor
+         */
+        RigidBody();
+
+        /**
+         * Set the mass
+         *
+         * @param mass
+         */
+        void setMass(float mass);
+
+        /**
+         * Get the mass
+         *
+         * @return
+         */
+        [[nodiscard]] float getMass() const;
+
+        /**
+         * Set the gravity
+         *
+         * @param gravity
+         */
+        void setGravity(float gravity);
+
+        /**
+         * Get the gravity
+         *
+         * @return
+         */
+        [[nodiscard]] float getGravity() const;
+
+        // -----------------------------------------------------------------------------
+        // Force
+        // -----------------------------------------------------------------------------
+
+        /**
+         * Apply a force on this body
+         *
+         * @param force
+         */
+        void applyForce(const Vector2D& force);
+
+        /**
+         * Apply a force on the x-axis
+         *
+         * @param force
+         */
+        void applyForceX(float force);
+
+        /**
+         * Apply a force on the y-axis
+         *
+         * @param force
+         */
+        void applyForceY(float force);
+
+        /**
+         * Remove force
+         */
+        void unsetForce();
+
+        // -----------------------------------------------------------------------------
+        // Friction
+        // -----------------------------------------------------------------------------
+
+        /**
+         * Apply friction on body
+         *
+         * @param friction
+         */
+        void applyFriction(const Vector2D& friction);
+
+        /**
+         * Remove friction
+         */
+        void unsetFriction();
+
+        // -----------------------------------------------------------------------------
+        // Position
+        // -----------------------------------------------------------------------------
+
+        /**
+         * Get the position of this body
+         *
+         * @return
+         */
+        [[nodiscard]] Vector2D getPosition() const;
+
+        // -----------------------------------------------------------------------------
+        // Velocity
+        // -----------------------------------------------------------------------------
+
+        /**
+         * Get the velocity of this body
+         *
+         * @return
+         */
+        [[nodiscard]] Vector2D getVelocity() const;
+
+        // -----------------------------------------------------------------------------
+        // Acceleration
+        // -----------------------------------------------------------------------------
+
+        /**
+         * Get the acceleration of this body
+         *
+         * @return
+         */
+        [[nodiscard]] Vector2D getAcceleration() const;
+
+        // -----------------------------------------------------------------------------
+        // Update
+        // -----------------------------------------------------------------------------
+
+        /**
+         * Update this body
+         *
+         * Method computes the body's acceleration, velocity and position.
+         *
+         * @param delta
+         */
+        void update(float delta);
+
+    protected:
+        /**
+         * The mass of this body
+         */
+        float mass;
+
+        /**
+         * The gravity that affects this body
+         */
+        float gravity;
+
+        /**
+         * Force to be applied on the x and y-axis
+         */
+        Vector2D force;
+
+        /**
+         * Friction that affects the body
+         */
+        Vector2D friction;
+
+        /**
+         * The position of the body
+         */
+        Vector2D position;
+
+        /**
+         * The velocity (speed) of the body
+         */
+        Vector2D velocity;
+
+        /**
+         * The acceleration of the body
+         */
+        Vector2D acceleration;
+};
+
+#endif  // PHYSICS_RIGID_BODY_H
---
+++ src/Physics/RigidBody.cpp
@@ -0,0 +1,110 @@
+#include "Physics/RigidBody.h"
+#include "Physics/Vector2D.h"
+
+RigidBody::RigidBody():
+    mass(DEFAULT_MASS),
+    gravity(DEFAULT_GRAVITY)
+{
+
+}
+
+void RigidBody::setMass(const float mass)
+{
+    this->mass = mass;
+}
+
+float RigidBody::getMass() const
+{
+    return this->mass;
+}
+
+void RigidBody::setGravity(const float gravity)
+{
+    this->gravity = gravity;
+}
+
+float RigidBody::getGravity() const
+{
+    return this->gravity;
+}
+
+// -----------------------------------------------------------------------------
+// Force
+// -----------------------------------------------------------------------------
+
+void RigidBody::applyForce(const Vector2D& force)
+{
+    this->force = force;
+}
+
+void RigidBody::applyForceX(const float force)
+{
+    this->force.x = force;
+}
+void RigidBody::applyForceY(const float force)
+{
+    this->force.y = force;
+}
+
+void RigidBody::unsetForce()
+{
+    this->force = Vector2D(0, 0);
+}
+
+// -----------------------------------------------------------------------------
+// Friction
+// -----------------------------------------------------------------------------
+
+void RigidBody::applyFriction(const Vector2D& friction)
+{
+    this->friction = friction;
+}
+
+void RigidBody::unsetFriction()
+{
+    this->friction = Vector2D(0, 0);
+}
+
+// -----------------------------------------------------------------------------
+// Position
+// -----------------------------------------------------------------------------
+
+Vector2D RigidBody::getPosition() const
+{
+    return this->position;
+}
+
+// -----------------------------------------------------------------------------
+// Velocity
+// -----------------------------------------------------------------------------
+
+Vector2D RigidBody::getVelocity() const
+{
+    return this->velocity;
+}
+
+// -----------------------------------------------------------------------------
+// Acceleration
+// -----------------------------------------------------------------------------
+
+Vector2D RigidBody::getAcceleration() const
+{
+    return this->acceleration;
+}
+
+// -----------------------------------------------------------------------------
+// Update
+// -----------------------------------------------------------------------------
+
+void RigidBody::update(const float delta)
+{
+    // Compute the acceleration for x and y-axis
+    this->acceleration.x = (this->force.x + this->friction.x) / this->mass;
+    this->acceleration.y = (this->gravity + this->force.y) / this->mass;
+
+    // Compute the velocity
+    this->velocity = this->acceleration * delta;
+
+    // Finally, compute the position
+    this->position = this->velocity * delta;
+}
