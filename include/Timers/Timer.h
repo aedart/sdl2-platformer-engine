8f12e2f1c0cbf7711c95202517326e2c4b17c9b7
alin <alin@rspsystems.com>
2024-12-26 16:12:13 +0100

    Add delta time logic
    Delta time is now computed with upper limits for targeted frames per second, as well as a targeted (max) delta time.

--- include/Timers/Timer.h
+++ include/Timers/Timer.h
@@ -10,6 +10,16 @@ class Timer
 {
     public:
         /**
+         * Default targeted frames per second
+         */
+        static constexpr int DEFAULT_TARGET_FPS = 60;
+
+        /**
+         * Default targeted delta time (in milliseconds)
+         */
+        static constexpr float DEFAULT_TARGET_DELTA_TIME = 1.5f;
+
+        /**
          * Prevent copy constructor
          */
         Timer(const Timer&) = delete;
@@ -38,7 +48,73 @@ class Timer
          */
         virtual ~Timer();
 
+        /**
+         * Computes the delta time
+         */
+        void tick();
+
+        /**
+         * Returns the time past since last tick
+         *
+         * @return Delta time in milliseconds
+         */
+        [[nodiscard]] float getDeltaTime() const;
+
+        /**
+         * Get the time of the last tick
+         *
+         * @return
+         */
+        [[nodiscard]] float getLastTime() const;
+
+        /**
+         * Set the targeted frames per second
+         *
+         * @param fps
+         */
+        void setTargetFPS(int fps);
+
+        /**
+         * Get the targeted frames per second
+         *
+         * @return
+         */
+        [[nodiscard]] int getTargetFPS() const;
+
+        /**
+         * Set the targeted delta time
+         *
+         * @param deltaTime Time in milliseconds
+         */
+        void setTargetDeltaTime(float deltaTime);
+
+        /**
+         * Get the targeted delta time
+         *
+         * @return Target delta time in milliseconds
+         */
+        [[nodiscard]] float getTargetDeltaTime() const;
+
     protected:
+        /**
+         * The past between two ticks, in milliseconds
+         */
+        float deltaTime;
+
+        /**
+         * Time of last tick
+         */
+        float lastTime;
+
+        /**
+         * The targeted frames per second
+         */
+        int targetFps;
+
+        /**
+         * The targeted delta time, in milliseconds
+         */
+        float targetDeltaTime;
 
         /**
          * Default constructor
@@ -52,4 +128,4 @@ class Timer
         inline static Timer* instance = nullptr;
 };
 
-#endif // TIMERS_TIMER_H
+#endif  // TIMERS_TIMER_H
--- src/Timers/Timer.cpp
+++ src/Timers/Timer.cpp
@@ -1,3 +1,4 @@
+#include <SDL2/SDL.h>
 #include "Timers/Timer.h"
 
 Timer& Timer::getInstance()
@@ -19,8 +20,62 @@ Timer::~Timer()
     instance = nullptr;
 }
 
+void Timer::tick()
+{
+    // Obtain the difference since last tick, and scale it down according
+    // to the specified targeted frames per second.
+    auto deltaTime = (SDL_GetTicks64() - this->lastTime) * (this->getTargetFPS() / 1000.0f);
+
+    // Limit the delta time accordingly
+    if (deltaTime > this->getTargetDeltaTime()) {
+        deltaTime = this->getTargetDeltaTime();
+    }
+
+    // Set the current delta time and update the last tick time
+    this->deltaTime = deltaTime;
+    this->lastTime = SDL_GetTicks64();
+}
+
+float Timer::getDeltaTime() const
+{
+    return this->deltaTime;
+}
+
+float Timer::getLastTime() const
+{
+    return this->lastTime;
+}
+
+void Timer::setTargetFPS(const int fps)
+{
+    this->targetFps = fps;
+}
+
+int Timer::getTargetFPS() const
+{
+    return this->targetFps;
+}
+
+void Timer::setTargetDeltaTime(const float deltaTime)
+{
+    this->targetDeltaTime = deltaTime;
+}
+
+float Timer::getTargetDeltaTime() const
+{
+    return this->targetDeltaTime;
+}
+
 // -----------------------------------------------------------------------------
 // Internals
 // -----------------------------------------------------------------------------
 
-Timer::Timer() = default;
+Timer::Timer():
+    lastTime(SDL_GetTicks64()),
+    targetFps(Timer::DEFAULT_TARGET_FPS),
+    targetDeltaTime(Timer::DEFAULT_TARGET_DELTA_TIME)
+{
+    // Ensure that the initial delta time is set, in case of
+    // the first run of a game loop.
+    this->tick();
+}
