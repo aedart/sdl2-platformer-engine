22a03f1495774612962fc4d6baca332d235441f7
alin <alin@rspsystems.com>
2024-12-26 17:32:06 +0100

    Add Tileset DTO

---
+++ include/Maps/Tileset.h
@@ -0,0 +1,59 @@
+#ifndef MAPS_TILESET_H
+#define MAPS_TILESET_H
+
+#include <string>
+
+/**
+ * Tileset
+ *
+ * Data structure representation of a tileset
+ *
+ * @see http://www.mapeditor.org/
+ *
+ * @author Alin Eugen Deac <aedart@gmail.com>
+ */
+class Tileset
+{
+    public:
+        /**
+         * Name of this tileset
+         */
+        std::string name;
+
+        /**
+         * The source texture file of this tileset
+         */
+        std::string source;
+
+        /**
+         * ID of the first tile
+         */
+        int firstID;
+
+        /**
+         * ID of the last tile
+         */
+        int lastID;
+
+        /**
+         * Amount of rows in tileset
+         */
+        int rows;
+
+        /**
+         * Amount of columns in tileset
+         */
+        int columns;
+
+        /**
+         * Total amount of tiles in this tileset
+         */
+        int tilesCount;
+
+        /**
+         * The pixel size of the tiles
+         */
+        int tilesSize;
+};
+
+#endif  // MAPS_TILESET_H
---
+++ src/Maps/Tileset.cpp
@@ -0,0 +1,1 @@
+#include "Maps/Tileset.h"
