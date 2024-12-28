f310ea90127f4ca6b2e7d5b85c6bbcbc2be37692
alin <alin@rspsystems.com>
2024-12-28 20:18:24 +0100

    Add camera update logic
    This will change in a future version, but good enough for now.

--- include/Cameras/Camera.h
+++ include/Cameras/Camera.h
@@ -1,6 +1,10 @@
 #ifndef CAMERAS_CAMERA_H
 #define CAMERAS_CAMERA_H
 
+#include <SDL2/SDL.h>
+#include "Physics/Point.h"
+#include "Physics/Vector2D.h"
+
 /**
  * Camera
  *
@@ -38,7 +42,50 @@ class Camera
          */
         virtual ~Camera();
 
+        /**
+         * Get the camera's view box
+         *
+         * @return A rect that defines the visible area of the camera
+         */
+        [[nodiscard]] SDL_Rect getViewBox() const;
+
+        /**
+         * Get this camera's current position
+         *
+         * @return
+         */
+        [[nodiscard]] Vector2D getPosition() const;
+
+        /**
+         * Set this camera's targeted point
+         *
+         * @param point
+         */
+        void setTarget(Point* point);
+
+        /**
+         * Update this camera
+         *
+         * @param delta
+         */
+        void update(float delta);
+
     protected:
+        /**
+         * The target point of this camera
+         */
+        Point* target;
+
+        /**
+         * Current position of the camera
+         */
+        Vector2D position;
+
+        /**
+         * The camera's visible area
+         */
+        SDL_Rect viewBox;
+
         /**
          * Default constructor
          */
--- src/Cameras/Camera.cpp
+++ src/Cameras/Camera.cpp
@@ -1,3 +1,5 @@
+#include "Core/Engine.h"
+
 #include "Cameras/Camera.h"
 
 Camera& Camera::getInstance()
@@ -15,12 +17,72 @@ void Camera::destroy()
 
 Camera::~Camera()
 {
+    delete this->target;
+
     instance = nullptr;
 }
 
+SDL_Rect Camera::getViewBox() const
+{
+    return this->viewBox;
+}
+
+Vector2D Camera::getPosition() const
+{
+    return this->position;
+}
+
+void Camera::setTarget(Point* point)
+{
+    this->target = point;
+}
+
+void Camera::update(float delta)
+{
+    if (this->target == nullptr) {
+        return;
+    }
+
+    // TODO: Handling of delta?...
+    // TODO: ...also, these are some pretty hard-coded width / height values !?
+
+    const auto& engine = Engine::getInstance();
+    const auto screenWidth = engine.getScreenWidth();
+    const auto screenHeight = engine.getScreenHeight();
+
+    this->viewBox.x = this->target->x - screenWidth / 2;
+    this->viewBox.y = this->target->y - screenHeight / 2;
+
+    if (this->viewBox.x < 0) {
+        this->viewBox.x = 0;
+    }
+
+    if (this->viewBox.y < 0) {
+        this->viewBox.y = 0;
+    }
+
+    const auto maxWidth = (2 * screenWidth - this->viewBox.w);
+    if (this->viewBox.x > maxWidth) {
+        this->viewBox.x = maxWidth;
+    }
+
+    const auto maxHeight = (screenHeight - this->viewBox.h);
+    if (this->viewBox.y > maxHeight) {
+        this->viewBox.y = maxHeight;
+    }
+
+    this->position = Vector2D(this->viewBox.x, this->viewBox.y);
+}
 
 // -----------------------------------------------------------------------------
 // Internals
 // -----------------------------------------------------------------------------
 
-Camera::Camera() = default;
+Camera::Camera():
+    target(nullptr),
+    position(),
+
+    // Initialise the view box according to the engine's screen width and height.
+    viewBox(0, 0, Engine::getInstance().getScreenWidth(), Engine::getInstance().getScreenHeight())
+{
+}
