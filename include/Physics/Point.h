e3e74d754c02dc8cd4cd45214111e93cc83165ef
alin <alin@rspsystems.com>
2024-12-28 19:41:30 +0100

    Add Point

---
+++ include/Physics/Point.h
@@ -0,0 +1,86 @@
+#ifndef PHYSICS_POINT_H
+#define PHYSICS_POINT_H
+
+#include <iostream>
+
+/**
+ * Point
+ *
+ * @author Alin Eugen Deac <aedart@gmail.com>
+ */
+class Point
+{
+    public:
+        /**
+         * X Position
+         */
+        float x;
+
+        /**
+         * Y Position
+         */
+        float y;
+
+        /**
+         * Constructor
+         * @param x X position
+         * @param y Y position
+         */
+        explicit Point(float x = 0, float y = 0);
+
+        /**
+         * Addition operator overloading
+         *
+         * @param point
+         *
+         * @return
+         */
+        Point operator+(const Point& point) const;
+
+        /**
+         * Addition operator overloading
+         *
+         * @param a Point a
+         * @param b Point b
+         *
+         * @return
+         */
+        Point operator+=(Point& a, const Point& b) const;
+
+        /**
+         * Subtraction operator overloading
+         *
+         * @param point
+         *
+         * @return
+         */
+        Point operator-(const Point& point) const;
+
+        /**
+         * Subtraction operator overloading
+         *
+         * @param a Point a
+         * @param b Point b
+         *
+         * @return
+         */
+        Point operator-=(Point& a, const Point& b) const;
+
+        /**
+         * Multiplication operator overloading
+         *
+         * @param scalar
+         *
+         * @return
+         */
+        Point operator*(float scalar) const;
+
+        /**
+         * Outputs point's coordinates and given message to stdout
+         *
+         * @param message
+         */
+        void print(const std::string& message = "") const;
+};
+
+#endif  // PHYSICS_POINT_H
---
+++ src/Physics/Point.cpp
@@ -0,0 +1,42 @@
+#include "Physics/Point.h"
+
+Point::Point(const float x, const float y):
+    x(x),
+    y(y)
+{}
+
+Point Point::operator+(const Point& point) const
+{
+    return Point(this->x + point.x, this->y + point.y);
+}
+
+Point Point::operator+=(Point& a, const Point& b) const
+{
+    a.x += b.x;
+    a.y += b.y;
+
+    return a;
+}
+
+Point Point::operator-(const Point& point) const
+{
+    return Point(this->x - point.x, this->y - point.y);
+}
+
+Point Point::operator-=(Point& a, const Point& b) const
+{
+    a.x -= b.x;
+    a.y -= b.y;
+
+    return a;
+}
+
+Point Point::operator*(const float scalar) const
+{
+    return Point(this->x * scalar, this->y * scalar);
+}
+
+void Point::print(const std::string& message) const
+{
+    std::cout << message << ": (" << this->x << ", " << this->y << ")" << std::endl;
+}
