ae6e2e65f2e7d66f013bee3baeacffab86230cb5
alin <alin@rspsystems.com>
2024-12-29 16:26:52 +0100

    Add collision check methods
    The implementation is still lacking a better way of determining / setting which layer is the "collision" layer of the current map. AT the moment it has been hard-coded, which is NOT very flexible at all...

--- include/Collisions/CollisionHandler.h
+++ include/Collisions/CollisionHandler.h
@@ -1,6 +1,10 @@
 #ifndef COLLISIONS_COLLISION_HANDLER_H
 #define COLLISIONS_COLLISION_HANDLER_H
 
+#include <SDL2/SDL.h>
+#include <vector>
+#include "Maps/TileLayer.h"
+
 /**
  * Collision Handler
  *
@@ -38,7 +42,37 @@ class CollisionHandler
          */
         virtual ~CollisionHandler();
 
+        /**
+         * Determine if there is a collision between rect a and b
+         *
+         * @param a First rect
+         * @param b Second rect
+         *
+         * @return
+         */
+        bool checkCollision(const SDL_Rect& a, const SDL_Rect& b);
+
+        /**
+         * Determine if there is a collision between given rect and
+         * the map's collision layer
+         *
+         * @param a The rect to check against
+         *
+         * @return
+         */
+        [[nodiscard]] bool mapCollision(const SDL_Rect& a) const;
+
     protected:
+        /**
+         * The actual tilemap (matrix) that contains
+         * collision tiles.
+         */
+        TileMap layerTileMap;
+
+        /**
+         * The collision layer
+         */
+        TileLayer* collisionLayer;
 
         /**
          * Default constructor
--- src/Collisions/CollisionHandler.cpp
+++ src/Collisions/CollisionHandler.cpp
@@ -1,4 +1,6 @@
 #include "Collisions/CollisionHandler.h"
+#include "Maps/TileLayer.h"
+#include "Core/Engine.h"
 
 CollisionHandler& CollisionHandler::getInstance()
 {
@@ -19,8 +21,76 @@ CollisionHandler::~CollisionHandler()
     instance = nullptr;
 }
 
+bool CollisionHandler::checkCollision(const SDL_Rect& a, const SDL_Rect& b)
+{
+    const bool overlapsX = (a.x < b.x + b.w) && (a.x + a.w > b.x);
+    const bool overlapsY = (a.y < b.y + b.h) && (a.y + a.h > b.y);
+
+    return overlapsX && overlapsY;
+}
+
+bool CollisionHandler::mapCollision(const SDL_Rect& a) const
+{
+    // TODO: Seriously!!!
+    const int tileSize = 32;
+
+    const int width = this->collisionLayer->getTileWidth();
+    const int height = this->collisionLayer->getTileHeight();
+    const int rows = this->collisionLayer->getRows();
+    const int columns = this->collisionLayer->getColumns();
+
+    int leftTile = a.x / width;
+    int rightTile = (a.x + a.w) / width;
+
+    int topTile = a.y / height;
+    int bottomTile = (a.y + a.h) / height;
+
+    // Limit left tile
+    if (leftTile < 0) {
+        leftTile = 0;
+    }
+
+    // Limit right tile
+    if (rightTile > columns) {
+        rightTile = columns;
+    }
+
+    // Limit top tile
+    if (topTile < 0) {
+        topTile = 0;
+    }
+
+    // Limit bottom tile
+    if (bottomTile > rows) {
+        bottomTile = rows;
+    }
+
+    // Determine if there is a collision
+    for (int i = leftTile; i <= rightTile; i++) {
+        for (int j = topTile; j <= bottomTile; j++) {
+
+            // A collision is only valid, if the tile ID is not zero!
+            if (this->layerTileMap[j][i] > 0) {
+                return true;
+            }
+        }
+    }
+
+    return false;
+}
+
 // -----------------------------------------------------------------------------
 // Internals
 // -----------------------------------------------------------------------------
 
-CollisionHandler::CollisionHandler() = default;
+CollisionHandler::CollisionHandler()
+{
+    this->collisionLayer = dynamic_cast<TileLayer*>(
+        // TODO: This is really poor - we should be able to determine / set
+        // TODO: what the collision layer is intended to be, and not just
+        // TODO: have a hard-coded value here!
+        Engine::getInstance().getCurrentMap()->layers.back()
+    );
+
+    this->layerTileMap = this->collisionLayer->getTileMap();
+}
