7fdb2819a6196f10f3d32db02511da77f59513d0
alin <alin@rspsystems.com>
2025-01-01 16:46:57 +0100

    Add the game state abstract methods

--- include/States/GameState.h
+++ include/States/GameState.h
@@ -4,11 +4,34 @@
 /**
  * Game State
  *
+ * @abstract
+ *
  * @author Alin Eugen Deac <aedart@gmail.com>
  */
 class GameState
 {
+    public:
+        /**
+         * Initialise this state
+         *
+         * @return
+         */
+        virtual bool init() = 0;
 
+        /**
+         * Exit this state
+         */
+        virtual void exit() = 0;
+
+        /**
+         * Update this state
+         */
+        virtual void update() = 0;
+
+        /**
+         * Render this state
+         */
+        virtual void render() = 0;
 };
 
 #endif // STATES_GAME_STATE_H
