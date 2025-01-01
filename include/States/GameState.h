29c5dee140d832cd5392cf88e0eedc373f3930d6
alin <alin@rspsystems.com>
2025-01-01 16:31:17 +0100

    Add Game State (incomplete)
    Abstraction for the game's overall state

---
+++ include/States/GameState.h
@@ -0,0 +1,14 @@
+#ifndef STATES_GAME_STATE_H
+#define STATES_GAME_STATE_H
+
+/**
+ * Game State
+ *
+ * @author Alin Eugen Deac <aedart@gmail.com>
+ */
+class GameState
+{
+
+};
+
+#endif // STATES_GAME_STATE_H
