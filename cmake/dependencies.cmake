7fd911bb2024ec5009863f7567a6b5ff26546d4f
alin <alin@rspsystems.com>
2025-01-06 15:38:49 +0100

    Refactor cmake scripts for handling dependencies
    Moved inside the cmake directory to avoid polluting the root directory structure.

--- cmake/dependencies.cmake
+++ cmake/dependencies.cmake
@@ -3,12 +3,15 @@
 # --------------------------------------------------------------------------------------------------
 
 # Debug
-message("Resolving dependencies for ${PROJECT_NAME}")
+message(STATUS "Resolving dependencies for ${PROJECT_NAME}")
+
+set(DEPENDENCIES_DIR "${CMAKE_SOURCE_DIR}/cmake/dependencies")
+list(APPEND CMAKE_MODULE_PATH "${DEPENDENCIES_DIR}")
 
 # Ensure that git is available
 find_package(Git REQUIRED)
 
-# Other dependencies...
+# External dependencies...
 include("sdl2")
 include("sdl2-image")
 include("sdl2-ttf")
---
+++ cmake/dev-dependencies.cmake
@@ -0,0 +1,11 @@
+# --------------------------------------------------------------------------------------------------
+# Loads all 3rd party development dependencies
+#
+# - Use this instead of the "dependencies" module script, if you need development dependencies...
+# --------------------------------------------------------------------------------------------------
+
+# Ensure the regular dependencies are installed
+include("dependencies")
+
+# List of additional development dependencies
+# ...N/A
